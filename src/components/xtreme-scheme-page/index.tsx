1d259feaed524593addedd9b96d4b0947fb12230
Ильшат Исмагилов <starkylife@Ilsats-MacBook-Air.local>
2021-04-16 16:30:37 +0300

    refactor(*): cleanup scheme page

--- src/components/xtreme-scheme-page/index.tsx
+++ src/components/xtreme-scheme-page/index.tsx
@@ -8,45 +8,104 @@ import { AppRoutes } from '../../app-routes';
 
 import './styles.css';
 
-type Practice = { name: string; description: string; path?: string };
+type Practice = {
+    name: string;
+    description: string;
+    path?: string;
+    position: 'top' | 'right' | 'bottom' | 'left' | 'top-right' | 'bottom-right' | 'bottom-left' | 'top-left';
+};
 
-const practices: Practice[] = [
-    { name: 'Whole Team', description: 'Вся команда сидит вместе и работает над одной задачей' },
-    { name: 'Planning Games', description: 'Игры планирования (planning poker и др.)' },
-    { name: 'Small Releases', description: 'Раскатка на бой маленькими кусочками' },
-    { name: 'Acceptance Tests', description: 'Автоматизированные приемочные тесты', path: AppRoutes.BEHAVIOR_DRIVEN_DEVELOPMENT_PAGE },
-    { name: 'Collective Code Ownership', description: 'Коллективное владение кодом (будь то LoanMB, фронт или мидл)' },
-    { name: 'Sustainable Pace', description: 'Устойчивый темп' },
-    { name: 'Metaphor', description: 'a story that everyone - customers, programmers, and managers - can tell about how the system works' },
-    { name: 'Continuous Integration', description: 'Develop -> Build -> Test end-to-end -> Stage (https://www.scaledagileframework.com/continuous-integration/)' },
-    { name: 'TDD', description: 'Разработка через тестирование', path: AppRoutes.TEST_DRIVEN_DEVELOPMENT_PAGE },
-    { name: 'Refactoring', description: 'Улучшение кодовой базы' },
-    { name: 'Simple Design', description: 'Простой и понятный дизайн системы' },
-    { name: 'Pair Programming', description: 'Парное программирование', path: AppRoutes.PAIR_PROGRAMMING_PAGE },
+const businessPractices: Practice[] = [
+    {
+        name: 'Whole Team',
+        description: 'Вся команда сидит вместе и работает над одной задачей',
+        position: 'top',
+    },
+    {
+        name: 'Planning Games',
+        description: 'Игры планирования (planning poker и др.)',
+        position: 'right',
+    },
+    {
+        name: 'Small Releases',
+        description: 'Раскатка на бой маленькими кусочками',
+        position: 'bottom',
+    },
+    {
+        name: 'Acceptance Tests',
+        description: 'Автоматизированные приемочные тесты',
+        position: 'left',
+        path: AppRoutes.BEHAVIOR_DRIVEN_DEVELOPMENT_PAGE,
+    },
+];
+const teamPractices: Practice[] = [
+    {
+        name: 'Collective Code Ownership',
+        description: 'Коллективное владение кодом (будь то LoanMB, фронт или мидл)',
+        position: 'top-right',
+    },
+    {
+        name: 'Sustainable Pace',
+        description: 'Устойчивый темп',
+        position: 'bottom-right',
+    },
+    {
+        name: 'Metaphor',
+        description: 'a story that everyone - customers, programmers, and managers - can tell about how the system works',
+        position: 'bottom-left',
+    },
+    {
+        name: 'Continuous Integration',
+        description: 'Develop -> Build -> Test end-to-end -> Stage (https://www.scaledagileframework.com/continuous-integration/)',
+        position: 'top-left',
+    },
+];
+const engineeringPractices: Practice[] = [
+    {
+        name: 'TDD',
+        description: 'Разработка через тестирование',
+        path: AppRoutes.TEST_DRIVEN_DEVELOPMENT_PAGE,
+        position: 'top',
+    },
+    {
+        name: 'Refactoring',
+        description: 'Улучшение кодовой базы',
+        position: 'right',
+    },
+    {
+        name: 'Simple Design',
+        description: 'Простой и понятный дизайн системы',
+        position: 'bottom',
+    },
+    {
+        name: 'Pair Programming',
+        description: 'Парное программирование',
+        path: AppRoutes.PAIR_PROGRAMMING_PAGE,
+        position: 'left',
+    },
 ];
 
-const SchemeElement: React.FC<{
-    practice: Practice;
-    className?: string;
-}> = ({
-    practice,
-    className,
+const SchemeElement: React.FC<Practice> = ({
+    name,
+    description,
+    path,
+    position,
 }) => {
     const history = useHistory();
 
     return (
         <Tooltip
-            key={ practice.name }
-            content={ practice.description }
+            key={ name }
+            content={ description }
             position="top"
-            targetClassName={ `tooltip-container${className ? ` ${className}` : ''}` }
+            targetClassName={ `scheme-element ${position}` }
         >
             <Button
                 view="secondary"
-                style={ { background: 'white' } }
-                onClick={ () => practice.path && history.push(practice.path) }
+                className="scheme-element__button"
+                onClick={ () => path && history.push(path) }
             >
-                { practice.name }
+                { name }
             </Button>
         </Tooltip>
     );
@@ -55,61 +114,17 @@ const SchemeElement: React.FC<{
 export const XtremeSchemePage: React.FC = () => (
     <section className="scheme">
         <div className="circle-container business">
-            <SchemeElement
-                className="scheme-element top"
-                practice={ practices[0] }
-            />
-            <SchemeElement
-                className="scheme-element right"
-                practice={ practices[1] }
-            />
-            <SchemeElement
-                className="scheme-element bottom"
-                practice={ practices[2] }
-            />
-            <SchemeElement
-                className="scheme-element left"
-                practice={ practices[3] }
-            />
-            <div className="circle blue">
-                <div className="circle-container team">
-                    <SchemeElement
-                        className="scheme-element top-right"
-                        practice={ practices[4] }
-                    />
-                    <SchemeElement
-                        className="scheme-element bottom-right"
-                        practice={ practices[5] }
-                    />
-                    <SchemeElement
-                        className="scheme-element bottom-left"
-                        practice={ practices[6] }
-                    />
-                    <SchemeElement
-                        className="scheme-element top-left"
-                        practice={ practices[7] }
-                    />
-                    <div className="circle orange">
-                        <div className="circle-container engineering">
-                            <SchemeElement
-                                className="scheme-element top"
-                                practice={ practices[8] }
-                            />
-                            <SchemeElement
-                                className="scheme-element right"
-                                practice={ practices[9] }
-                            />
-                            <SchemeElement
-                                className="scheme-element bottom"
-                                practice={ practices[10] }
-                            />
-                            <SchemeElement
-                                className="scheme-element left"
-                                practice={ practices[11] }
-                            />
-                            <div className="circle green" />
-                        </div>
-                    </div>
+            { businessPractices.map((p) => (
+                <SchemeElement { ...p } />
+            )) }
+            <div className="circle-container team">
+                { teamPractices.map((p) => (
+                    <SchemeElement { ...p } />
+                )) }
+                <div className="circle-container engineering">
+                    { engineeringPractices.map((p) => (
+                        <SchemeElement { ...p } />
+                    )) }
                 </div>
             </div>
         </div>
